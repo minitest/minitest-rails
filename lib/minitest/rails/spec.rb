07a87fdcc24952843d86d9fca9140b8655156a65
Mike Moore <mike@blowmage.com>
2011-06-03 08:56:01 -0600

    Consolidate on MiniTest::Rails::Spec
    Mailers don't have an identifiable suffix, so always use class in tests.
    Make Rails test classes inherit from Spec.

--- lib/minitest/rails/spec.rb
+++ lib/minitest/rails/spec.rb
@@ -2,13 +2,9 @@ require "minitest/unit"
 require "minitest/spec"
 require "minitest/mock"
 
-require "active_support/testing/setup_and_teardown"
-
 module MiniTest
   module Rails
     class Spec < ::MiniTest::Spec
-      include ActiveSupport::Testing::SetupAndTeardown
-
       alias :method_name :__name__ if defined? :__name__
     end
   end
