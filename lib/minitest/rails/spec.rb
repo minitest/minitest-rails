28f727b2dfb2007ce66934f4cadbdea14ff7d9f2
Mike Moore <mike@blowmage.com>
2011-06-03 16:05:45 -0600

    Add SetupAndTeardown

--- lib/minitest/rails/spec.rb
+++ lib/minitest/rails/spec.rb
@@ -2,9 +2,13 @@ require "minitest/unit"
 require "minitest/spec"
 require "minitest/mock"
 
+require "active_support/testing/setup_and_teardown"
+
 module MiniTest
   module Rails
     class Spec < ::MiniTest::Spec
+      include ActiveSupport::Testing::SetupAndTeardown
+
       alias :method_name :__name__ if defined? :__name__
     end
   end
