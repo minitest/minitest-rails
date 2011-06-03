406058f2bdb8e650078a1ec0117ef991eb9eb783
Mike Moore <mike@blowmage.com>
2011-06-03 16:06:09 -0600

    Add fixture support

--- lib/minitest/rails/model.rb
+++ lib/minitest/rails/model.rb
@@ -1,14 +1,12 @@
 require "minitest/rails/spec"
-# require "active_support/testing/setup_and_teardown"
-# require "active_record/fixtures"
+require "active_record/fixtures"
 
 module MiniTest
   module Rails
     class Model < Spec
-      # include ActiveSupport::Testing::SetupAndTeardown
-      # include ActiveRecord::TestFixtures
-      #
-      # self.fixture_path = File.join(Rails.root, "test", "fixtures")
+      include ActiveRecord::TestFixtures
+
+      self.fixture_path = File.join(Rails.root, "test", "fixtures")
     end
   end
 end
