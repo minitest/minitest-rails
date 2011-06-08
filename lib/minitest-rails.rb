bab0c0f1f58f1379da8667416f9a2383c185e762
Mike Moore <mike@blowmage.com>
2011-06-08 10:28:27 -0600

    Update railtie
    Don't register as integration_tool until it exists.

--- lib/minitest-rails.rb
+++ lib/minitest-rails.rb
@@ -4,8 +4,8 @@ module MiniTest
   module Rails
     VERSION = "0.0.4"
     class Railtie < ::Rails::Railtie
-      config.app_generators.integration_tool :mini_test
-      config.app_generators.test_framework   :mini_test, :fixture => false
+      # config.app_generators.integration_tool :mini_test
+      config.app_generators.test_framework :mini_test
       config.app_generators.fallbacks[:mini_test] = :test_unit
 
       rake_tasks do
