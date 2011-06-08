fc56429e52f921a72738c7a46189ebeb1635cf87
Mike Moore <mike@blowmage.com>
2011-06-08 00:47:34 -0600

    Bump to 0.0.3

--- lib/minitest-rails.rb
+++ lib/minitest-rails.rb
@@ -2,7 +2,7 @@ require "rails"
 
 module MiniTest
   module Rails
-    VERSION = "0.0.2"
+    VERSION = "0.0.3"
     class Railtie < ::Rails::Railtie
       config.app_generators.integration_tool :mini_test
       config.app_generators.test_framework   :mini_test, :fixture => false
