4719887ff9ce1fc86ee13c0d127e19b3a2801fb2
Pete Higgins <pete@peterhiggins.org>
2011-08-30 20:32:28 -0700

    Remove broken code from helper test's setup.

--- lib/generators/mini_test/helper/templates/helper_spec.rb
+++ lib/generators/mini_test/helper/templates/helper_spec.rb
@@ -1,11 +1,9 @@
 require "minitest_helper"
 
 class <%= class_name %>HelperTest < MiniTest::Rails::Helper
-  before do
-    @helper= <%= class_name %>Helper.new
-  end
 
   it "must be a real test" do
     flunk "Need real tests"
   end
+
 end
