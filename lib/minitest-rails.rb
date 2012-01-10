424780ca2cb729617c67c9e161271bf70f503ebb
Rafał Wrzochol <rafal@wrzochol.net>
2012-01-10 18:21:20 +0100

    set integration_tool to :mini_test

--- lib/minitest-rails.rb
+++ lib/minitest-rails.rb
@@ -4,7 +4,7 @@ module MiniTest
   module Rails
     VERSION = "0.0.5"
     class Railtie < ::Rails::Railtie
-      # config.app_generators.integration_tool :mini_test
+      config.app_generators.integration_tool :mini_test
       config.app_generators.test_framework :mini_test
       config.app_generators.fallbacks[:mini_test] = :test_unit
 
