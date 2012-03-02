79386bb3863f8476680eb34ab6be03801f42ea42
Jack Chu <kamuigt@gmail.com>
2012-03-01 21:07:10 -0500

    Override #determine_default_controller_class in MiniTest::Rails::Controller to allow controller class arguments and nested describe blocks

--- lib/minitest/rails/controller.rb
+++ lib/minitest/rails/controller.rb
@@ -8,6 +8,17 @@ module MiniTest
       include ActiveSupport::Testing::SetupAndTeardown
       include ActionController::TestCase::Behavior
 
+      # Rails 3.2 determines the controller class by matching class names that end in Test
+      # This overides the #determine_default_controller_class method to allow you use Controller
+      # class names in your describe argument
+      def self.determine_default_controller_class(name)
+        if name.match(/.*(?:^|::)(\w+Controller)/)
+          $1.safe_constantize
+        else
+          super(name)
+        end
+      end
+
       before do
         @controller = self.class.name.match(/((.*)Controller)/)[1].constantize.new
         @routes     = ::Rails.application.routes
@@ -21,4 +32,3 @@ module MiniTest
 end
 
 MiniTest::Spec.register_spec_type /Controller/, MiniTest::Rails::Controller
-
