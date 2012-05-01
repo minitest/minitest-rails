3026e1ef91dcdc74d1fa0aec94b93134d0ed599d
Michael Schuerig <michael@schuerig.de>
2012-05-01 11:20:28 +0200

    Include necessary testing modules in helper spec.

--- lib/minitest/rails/helper.rb
+++ lib/minitest/rails/helper.rb
@@ -3,6 +3,8 @@ require "minitest/rails/spec"
 module MiniTest
   module Rails
     class Helper < Spec
+      include ActiveSupport::Testing::SetupAndTeardown
+      include ActionView::TestCase::Behavior
     end
   end
 end
