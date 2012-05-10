7d6194ad2d16ed7bf41c6e03fd741ab1d5d2a11a
Michael Schuerig <michael@schuerig.de>
2012-05-10 21:34:32 +0200

    Decompose Controller#controller into methods.

--- lib/minitest/rails/controller.rb
+++ lib/minitest/rails/controller.rb
@@ -13,63 +13,89 @@ module MiniTest
         @routes = ::Rails.application.routes
       end
 
-      # Define an anonymous controller class for testing.
-      #
-      # The purpose is to test abstract controllers and in particular
-      # ApplicationController. For the anonymous controller
-      # rendering of templates is disabled. During tests, the routes
-      # are redefined to
-      #
-      #   resources :anonymous
-      #
-      # base_controller - superclass for the anonymous controller.
-      #
-      # Examples
-      #
-      #   describe ApplicationController do
-      #     controller do
-      #       def index; end
-      #     end
-      #
-      #     it "assigns the current_user" do
-      #       get :index
-      #       assigns(:current_user).wont_be_nil
-      #     end
-      #   end
-      #
-      #
-      def self.controller(base_controller = ApplicationController, &block)
-        anonymous_controller_class = Class.new(base_controller) do
-          def render
-            # suppress rendering
-          end
+      class << self
 
-          def self.anonymous?
-            # Oh, the irony.
-            false
-          end
+        # Define a concrete anonymous controller class for testing.
+        #
+        # The purpose is to test abstract controllers and in particular
+        # ApplicationController. For the anonymous controller
+        # rendering of templates is disabled. During tests, the routes
+        # are redefined to
+        #
+        #   resources :anonymous
+        #
+        # base_controller - superclass for the anonymous controller.
+        #   If not given, the controller is take from the context.
+        #   If that doesn't work either, ApplicationController is used.
+        #
+        # Examples
+        #
+        #   describe ApplicationController do
+        #     controller
+        #
+        #     it "assigns the current_user" do
+        #       get :index
+        #       assigns(:current_user).wont_be_nil
+        #     end
+        #   end
+        #
+        #
+        def controller(base_controller = nil, &block)
+          anonymous = anonymous_controller_class(base_controller)
+          anonymous.class_eval(&block) if block_given?
+          setup_temporary_anonymous_routes
+          self.controller_class = anonymous
+        end
 
-          def self.name
-            # As of Rails 3.2.3, the name generated for anonymous controller
-            # classes is incorrect.
-            # See https://github.com/rails/rails/pull/6053
-            # This method, in conjunction with the overridden #anonymous?
-            # works around that issue.
-            'anonymous'
+        private
+
+        def anonymous_controller_class(base_controller = nil)
+          return @anonymous_controller_class if defined?(@anonymous_controller_class)
+
+          base_controller ||= described_controller
+
+          Class.new(base_controller) do
+            def index;    end
+            def show;     end
+            def new;      end
+            def create;   end
+            def edit;     end
+            def update;   end
+            def destroy;  end
+
+            def render
+              # suppress rendering
+            end
+
+            def self.anonymous?
+              false
+            end
+
+            def self.name
+              'anonymous'
+            end
           end
         end
 
-        anonymous_controller_class.class_eval(&block)
-
-        self.controller_class = anonymous_controller_class
-
-        before do
-          @orig_routes, @routes = @routes, ActionDispatch::Routing::RouteSet.new
-          @routes.draw { resources :anonymous }
+        def described_controller
+          if @desc.respond_to?(:<) && @desc <= ActionController::Base
+            @desc
+          else
+            ApplicationController
+          end
         end
 
-        after do
-          @routes, @orig_routes = @orig_routes, nil
+        def setup_temporary_anonymous_routes
+          return if defined?(@orig_routes) # We've been here already.
+
+          before do
+            @orig_routes, @routes = @routes, ActionDispatch::Routing::RouteSet.new
+            @routes.draw { resources :anonymous }
+          end
+
+          after do
+            @routes, @orig_routes = @orig_routes, nil
+          end
         end
       end
 
