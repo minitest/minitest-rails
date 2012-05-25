a649798c009ff02c0b76eb4166a96408e4d9b62c
Mike Moore <mike@blowmage.com>
2012-05-25 07:54:28 -0600

    Add routes to MT::R::AC::TestCase
    Also add the other test_helper code, including wiring up fixtures.

--- lib/minitest/rails.rb
+++ lib/minitest/rails.rb
@@ -1,3 +1,39 @@
+# Make double-sure the RAILS_ENV is not set to production,
+# so fixtures aren't loaded into that environment
+abort("Abort testing: Your Rails environment is running in production mode!") if Rails.env.production?
+
 require "minitest/rails/active_support"
 require "minitest/rails/action_controller"
 require "minitest/rails/action_mailer"
+
+# Enable turn if it is available
+begin
+  require 'turn'
+  MiniTest::Unit.use_natural_language_case_names = true
+rescue LoadError
+end
+
+if defined?(ActiveRecord::Base)
+  class MiniTest::Rails::ActiveSupport::TestCase
+    include ActiveRecord::TestFixtures
+    self.fixture_path = "#{Rails.root}/test/fixtures/"
+  end
+
+  # MiniTest::Rails::ActionDispatch::IntegrationTest.fixture_path = MiniTest::Rails::ActiveSupport::TestCase.fixture_path
+
+  def create_fixtures(*table_names, &block)
+    Fixtures.create_fixtures(MiniTest::Rails::ActiveSupport::TestCase.fixture_path, table_names, {}, &block)
+  end
+end
+
+class MiniTest::Rails::ActionController::TestCase
+  setup do
+    @routes = Rails.application.routes
+  end
+end
+
+# class MiniTest::Rails::ActionDispatch::IntegrationTest
+#   setup do
+#     @routes = Rails.application.routes
+#   end
+# end
