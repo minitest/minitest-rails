67289db8c579fb81be395e9454b5c81a3f9c3aef
Mike Moore <mike@blowmage.com>
2012-05-24 19:21:34 -0600

    Require Rails environment before MiniTest::Rails
    Getting load errors due to ActiveRecord not being available.

--- lib/generators/minitest/install/templates/test/minitest_helper.rb
+++ lib/generators/minitest/install/templates/test/minitest_helper.rb
@@ -1,11 +1,11 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../config/environment', __FILE__)
+
 require "minitest/autorun"
 require "minitest/rails"
 
 # Uncomment if you want awesome colorful output
 # require "minitest/pride"
-
-ENV["RAILS_ENV"] = "test"
-require File.expand_path('../../config/environment', __FILE__)
 
 class Minitest::Rails::Spec
   # Uncomment if you want to support fixtures for all specs
