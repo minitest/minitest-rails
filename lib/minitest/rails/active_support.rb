6876657d0b5757083027c15f3093b356ff3abb62
Sven G. Brönstrup <broenstrup@spom.net>
2012-06-04 16:10:43 +0200

    Only use AS::TestCase for the base class when describing a model when ActiveRecord::Base defined

--- lib/minitest/rails/active_support.rb
+++ lib/minitest/rails/active_support.rb
@@ -20,9 +20,11 @@ module MiniTest
           assert yield, msg
         end
 
-        # Use AS::TestCase for the base class when describing a model
-        register_spec_type(self) do |desc|
-          desc < ActiveRecord::Base
+        if defined?(ActiveRecord::Base)
+          # Use AS::TestCase for the base class when describing a model
+          register_spec_type(self) do |desc|
+            desc < ActiveRecord::Base
+          end
         end
 
         # For backward compatibility with Test::Unit
