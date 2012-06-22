322c8b091134cf799b122c2888d9bb38e4b50a62
blahed <tdunn13@gmail.com>
2012-06-22 16:07:02 -0400

    controller/helper generators now support namespaces: `rails generate controller admin/users'

--- lib/generators/mini_test/helper/helper_generator.rb
+++ lib/generators/mini_test/helper/helper_generator.rb
@@ -9,9 +9,9 @@ module MiniTest
 
       def create_test_files
         if options[:spec]
-          template "helper_spec.rb", "test/helpers/#{file_name}_helper_test.rb"
+          template "helper_spec.rb", File.join("test/helpers", class_path, "#{file_name}_helper_test.rb")
         else
-          template "helper_test.rb", "test/helpers/#{file_name}_helper_test.rb"
+          template "helper_test.rb", File.join("test/helpers", class_path, "#{file_name}_helper_test.rb")
         end
       end
     end
--- test/test_helper_generator.rb
+++ test/test_helper_generator.rb
@@ -25,6 +25,18 @@ class TestHelperGenerator < MiniTest::Unit::TestCase
     FileUtils.rm_r "test/helpers"
   end
 
+  def test_namespaced_helper_generator
+    text = capture(:stdout) do
+      MiniTest::Generators::HelperGenerator.start ["admin/user"]
+    end
+    assert_match(/create  test\/helpers\/admin\/user_helper_test.rb/m, text)
+    assert File.exists? "test/helpers/admin/user_helper_test.rb"
+    contents = open("test/helpers/admin/user_helper_test.rb").read
+    assert_match(/class Admin::UserHelperTest < MiniTest::Rails::Helper/m, contents)
+  ensure
+    FileUtils.rm_r "test/helpers"
+  end
+
   def test_helper_generator_spec
     text = capture(:stdout) do
       MiniTest::Generators::HelperGenerator.start ["user", "--spec"]
@@ -36,4 +48,17 @@ class TestHelperGenerator < MiniTest::Unit::TestCase
   ensure
     FileUtils.rm_r "test/helpers"
   end
+  
+  def test_helper_generator_spec
+    text = capture(:stdout) do
+      MiniTest::Generators::HelperGenerator.start ["admin/user", "--spec"]
+    end
+    assert_match(/create  test\/helpers\/admin\/user_helper_test.rb/m, text)
+    assert File.exists? "test/helpers/admin/user_helper_test.rb"
+    contents = open("test/helpers/admin/user_helper_test.rb").read
+    assert_match(/describe Admin::UserHelper do/m, contents)
+  ensure
+    FileUtils.rm_r "test/helpers"
+  end
+
 end
