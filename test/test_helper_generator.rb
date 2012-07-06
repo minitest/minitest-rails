962f5cb613cf45bd9edc32f5962ad9cb3ce25856
Mike Moore <mike@blowmage.com>
2012-07-06 01:44:23 -0600

    Update Helper generator tests

--- test/test_helper_generator.rb
+++ test/test_helper_generator.rb
@@ -18,7 +18,7 @@ class TestHelperGenerator < MiniTest::Unit::TestCase
     assert_match(/create  test\/helpers\/user_helper_test.rb/m, text)
     assert File.exists? "test/helpers/user_helper_test.rb"
     contents = open("test/helpers/user_helper_test.rb").read
-    assert_match(/class UserHelperTest < MiniTest::Rails::Helper/m, contents)
+    assert_match(/class UserHelperTest < MiniTest::Unit::TestCase/m, contents)
   ensure
     # TODO: Don"t write the files
     # I agree, it would be better to mock the file getting written
@@ -32,7 +32,7 @@ class TestHelperGenerator < MiniTest::Unit::TestCase
     assert_match(/create  test\/helpers\/admin\/user_helper_test.rb/m, text)
     assert File.exists? "test/helpers/admin/user_helper_test.rb"
     contents = open("test/helpers/admin/user_helper_test.rb").read
-    assert_match(/class Admin::UserHelperTest < MiniTest::Rails::Helper/m, contents)
+    assert_match(/class Admin::UserHelperTest < MiniTest::Unit::TestCase/m, contents)
   ensure
     FileUtils.rm_r "test/helpers"
   end
@@ -48,7 +48,7 @@ class TestHelperGenerator < MiniTest::Unit::TestCase
   ensure
     FileUtils.rm_r "test/helpers"
   end
-  
+
   def test_helper_generator_spec
     text = capture(:stdout) do
       MiniTest::Generators::HelperGenerator.start ["admin/user", "--spec"]
