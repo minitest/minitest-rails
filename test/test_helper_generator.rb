0729bf9c695eaf164d8d53a4c0d582d6e758e088
Mike Moore <mike@blowmage.com>
2012-07-14 10:53:11 -0600

    Cleanup my mess
    Fix some b0rkd tests and generate new Manifest and gemspec.

--- test/test_helper_generator.rb
+++ test/test_helper_generator.rb
@@ -18,7 +18,7 @@ class TestHelperGenerator < MiniTest::Unit::TestCase
     assert_match(/create  test\/helpers\/user_helper_test.rb/m, text)
     assert File.exists? "test/helpers/user_helper_test.rb"
     contents = open("test/helpers/user_helper_test.rb").read
-    assert_match(/class UserHelperTest < MiniTest::Unit::TestCase/m, contents)
+    assert_match(/class UserHelperTest < MiniTest::Rails::ActionView::TestCase/m, contents)
   ensure
     # TODO: Don"t write the files
     # I agree, it would be better to mock the file getting written
@@ -32,7 +32,7 @@ class TestHelperGenerator < MiniTest::Unit::TestCase
     assert_match(/create  test\/helpers\/admin\/user_helper_test.rb/m, text)
     assert File.exists? "test/helpers/admin/user_helper_test.rb"
     contents = open("test/helpers/admin/user_helper_test.rb").read
-    assert_match(/class Admin::UserHelperTest < MiniTest::Unit::TestCase/m, contents)
+    assert_match(/class Admin::UserHelperTest < MiniTest::Rails::ActionView::TestCase/m, contents)
   ensure
     FileUtils.rm_r "test/helpers"
   end
--- test/test_scaffold_generator.rb
+++ test/test_scaffold_generator.rb
@@ -15,10 +15,10 @@ class TestScaffoldGenerator < MiniTest::Unit::TestCase
     text = capture(:stdout) do
       MiniTest::Generators::ScaffoldGenerator.start ["user"]
     end
-    assert_match(/create  test\/controllers\/user_controller_test.rb/m, text)
-    assert File.exists? "test/controllers/user_controller_test.rb"
-    contents = open("test/controllers/user_controller_test.rb").read
-    assert_match(/class UserControllerTest/m, contents)
+    assert_match(/create  test\/controllers\/users_controller_test.rb/m, text)
+    assert File.exists? "test/controllers/users_controller_test.rb"
+    contents = open("test/controllers/users_controller_test.rb").read
+    assert_match(/class UsersControllerTest/m, contents)
   ensure
     # TODO: Don"t write the files
     # I agree, it would be better to mock the file getting written
@@ -29,10 +29,10 @@ class TestScaffoldGenerator < MiniTest::Unit::TestCase
     text = capture(:stdout) do
       MiniTest::Generators::ScaffoldGenerator.start ["user", "--spec"]
     end
-    assert_match(/create  test\/controllers\/user_controller_test.rb/m, text)
-    assert File.exists? "test/controllers/user_controller_test.rb"
-    contents = open("test/controllers/user_controller_test.rb").read
-    assert_match(/describe UserController do/m, contents)
+    assert_match(/create  test\/controllers\/users_controller_test.rb/m, text)
+    assert File.exists? "test/controllers/users_controller_test.rb"
+    contents = open("test/controllers/users_controller_test.rb").read
+    assert_match(/describe UsersController do/m, contents)
   ensure
     FileUtils.rm_r "test/controllers"
   end
