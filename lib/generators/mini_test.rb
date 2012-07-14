8778c44197d91431014775b0b9b73186e3eecf55
Mike Moore <mike@blowmage.com>
2012-07-13 18:10:48 -0600

    Fix generators in Rails 3.0
    Rails::Generators::NamedBase#module_namespacing didn't exist in Rails 3.0.
    This is why I was avoiding Rails 3.0...

--- lib/generators/mini_test.rb
+++ lib/generators/mini_test.rb
@@ -6,6 +6,13 @@ module MiniTest
       def self.source_root
         @_minitest_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'mini_test', generator_name, 'templates'))
       end
+
+      begin
+        # This method doesn't exist in Rails 3.0
+        def module_namespacing(&block)
+          yield if block_given?
+        end
+      end unless respond_to?(:module_namespacing)
     end
   end
 end
