1cec1f8ff74fdbba33ad0a09cc814a2212168f49
Mike Leary <mikeyfinn@gmail.com>
2012-10-08 21:41:38 -0700

    fix scaffold controller test to be green out of the box

--- lib/generators/mini_test/scaffold/templates/controller_test.rb
+++ lib/generators/mini_test/scaffold/templates/controller_test.rb
@@ -4,7 +4,7 @@ require "minitest_helper"
 class <%= controller_class_name %>ControllerTest < MiniTest::Rails::ActionController::TestCase
 
   before do
-    @<%= singular_table_name %> = <%= class_name %>.new
+    @<%= singular_table_name %> = <%= table_name %>(:one)
   end
 
   def test_index
@@ -20,30 +20,30 @@ class <%= controller_class_name %>ControllerTest < MiniTest::Rails::ActionContro
 
   def test_create
     assert_difference('<%= class_name %>.count') do
-      post :create, <%= key_value singular_table_name, "@#{singular_table_name}.attributes" %>
+      post :create, <%= "#{singular_table_name}: { #{attributes_hash} }" %>
     end
 
     assert_redirected_to <%= singular_table_name %>_path(assigns(:<%= singular_table_name %>))
   end
 
   def test_show
-    get :show, <%= key_value :id, "@#{singular_table_name}.to_param" %>
+    get :show, <%= key_value :id, "@#{singular_table_name}" %>
     assert_response :success
   end
 
   def test_edit
-    get :edit, <%= key_value :id, "@#{singular_table_name}.to_param" %>
+    get :edit, <%= key_value :id, "@#{singular_table_name}" %>
     assert_response :success
   end
 
   def test_update
-    put :update, <%= key_value :id, "@#{singular_table_name}.to_param" %>, <%= key_value singular_table_name, "@#{singular_table_name}.attributes" %>
+    put :update, <%= key_value :id, "@#{singular_table_name}" %>, <%= "#{singular_table_name}: { #{attributes_hash} }" %>
     assert_redirected_to <%= singular_table_name %>_path(assigns(:<%= singular_table_name %>))
   end
 
   def test_destroy
     assert_difference('<%= class_name %>.count', -1) do
-      delete :destroy, <%= key_value :id, "@#{singular_table_name}.to_param" %>
+      delete :destroy, <%= key_value :id, "@#{singular_table_name}" %>
     end
 
     assert_redirected_to <%= index_helper %>_path
