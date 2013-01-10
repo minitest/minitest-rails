bfa5313621655200362ccee4cfb80fc43550ccd8
Mike Moore <mike@blowmage.com>
2013-01-10 15:41:06 -0700

    Update generator base
    Backfill methods missing from different versions of Rails without version checking soup.

--- lib/generators/mini_test.rb
+++ lib/generators/mini_test.rb
@@ -3,24 +3,37 @@ require 'rails/generators/named_base'
 module MiniTest
   module Generators
     class Base < ::Rails::Generators::NamedBase #:nodoc:
+
+      class_option :old_style_hash, :type => :boolean, :default => false,
+                                    :desc => "Force using old style hash (:foo => 'bar') on Ruby >= 1.9"
+
       def self.source_root
         @_minitest_source_root ||= File.expand_path(File.join(File.dirname(__FILE__), 'mini_test', generator_name, 'templates'))
       end
 
-      if ::Rails.version.to_f < 3.1
-        protected
-        # These methods don't exist in Rails 3.0
-        def module_namespacing(&block)
-          yield if block_given?
+      protected
+
+      # Fill in methods that might be missing depending on the version of Rails
+
+      def module_namespacing(&block)
+        yield if block_given?
+      end unless respond_to? :module_namespacing
+
+      def key_value(key, value)
+        if options[:old_style_hash] || RUBY_VERSION < '1.9'
+          ":#{key} => #{value}"
+        else
+          "#{key}: #{value}"
         end
-        def key_value(key, value)
-          if options[:old_style_hash] || RUBY_VERSION < '1.9'
-            ":#{key} => #{value}"
-          else
-            "#{key}: #{value}"
-          end
+      end unless respond_to? :key_value
+
+      def attributes_names
+        @attributes_names ||= attributes.each_with_object([]) do |a, names|
+          names << a.column_name
+          names << "#{a.name}_type" if a.polymorphic?
         end
-      end
+      end unless respond_to? :attributes_names
+
     end
   end
 end
