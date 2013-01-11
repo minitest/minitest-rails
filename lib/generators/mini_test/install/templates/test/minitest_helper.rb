bf688c649eb802f0d970db73bc9616b7adfa26dd
Mike Moore <mike@blowmage.com>
2013-01-10 21:13:52 -0700

    Update capybara comment

--- lib/generators/mini_test/install/templates/test/minitest_helper.rb
+++ lib/generators/mini_test/install/templates/test/minitest_helper.rb
@@ -4,7 +4,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require "minitest/autorun"
 require "minitest/rails"
 
-# Uncomment if you want Capybara in accceptance/integration tests
+# Add `gem "minitest/rails/capybara"` to the test group of your Gemfile
+# and uncomment the following if you want Capybara feature tests
 # require "minitest/rails/capybara"
 
 # Uncomment if you want awesome colorful output
