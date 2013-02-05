264e02f2246a1f8a41609afbfeb7d34a5bd2648b
Mike Moore <mike@blowmage.com>
2013-02-05 08:42:53 -0700

    Fix test helper's instructions
    The gem name was wrong, with '/' in place of '-'.

--- lib/generators/mini_test/install/templates/test/minitest_helper.rb
+++ lib/generators/mini_test/install/templates/test/minitest_helper.rb
@@ -4,7 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require "minitest/autorun"
 require "minitest/rails"
 
-# Add `gem "minitest/rails/capybara"` to the test group of your Gemfile
+# Add `gem "minitest-rails-capybara"` to the test group of your Gemfile
 # and uncomment the following if you want Capybara feature tests
 # require "minitest/rails/capybara"
 
