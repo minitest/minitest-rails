c3cdb4cc1bcd8b2717190be5f838678dc402cc23
Mario Alberto Chavez <mario.chavez@gmail.com>
2013-04-09 22:57:51 -0500

    Setup minitest to run tests for Rails generators
    Register Rails::Generators::TestCase with minitest for testing Rails generators.

--- lib/minitest/rails.rb
+++ lib/minitest/rails.rb
@@ -95,6 +95,22 @@ class ActionDispatch::IntegrationTest
   register_spec_type(/(Integration|Acceptance)( ?Test)?\z/i, self)
 end
 
+require 'rails/generators/test_case'
+class Rails::Generators::TestCase
+  register_spec_type(self) do |desc|
+    Class === desc && desc < Rails::Generators::Base
+  end
+
+  register_spec_type(/Generator( ?Test)?\z/i, self)
+
+  def self.determine_default_generator(name)
+    generator = determine_constant_from_test_name(name) do |constant|
+      Class === constant && constant < Rails::Generators::Base
+    end
+    raise NameError.new("Unable to resolve generator for #{name}") if generator.nil?
+    generator
+  end
+end
 ################################################################################
 # 1.8.7 Spec DSL Support
 ################################################################################
