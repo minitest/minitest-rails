7064212dbab33eb744e2b0529c448f5f77299b7a
Mike Moore <mike@blowmage.com>
2013-07-08 12:35:35 -0600

    Add must/wont_change tests

--- test/rails/active_support/test_expectations.rb
+++ test/rails/active_support/test_expectations.rb
@@ -30,4 +30,20 @@ class TestActiveSupportExpectations < ActiveSupport::TestCase
     [1,2,3].wont_be_blank
     {:key => :value}.wont_be_blank
   end
+
+  def test_must_change
+    counter = 0
+
+    lambda { counter += 1 }.must_change "counter", +1
+
+    lambda { counter += 3 }.must_change "counter", +3
+
+    lambda { counter -= 1 }.must_change "counter", -1
+  end
+
+  def test_wont_change
+    counter = 0
+
+    lambda { counter = 0 }.wont_change "counter"
+  end
 end
