b612717083d5f8df23ec9bb3360c52acf187f9eb
Mario A Chavez <mario.chavez@gmail.com>
2013-08-02 14:14:11 -0500

    Extracted generator spec type from rails.rb file into it's own file, which will need to be included explicitly when testing generators with: require 'minitest/rails/generators' Also added spec type test for generator spec type.

--- lib/minitest/rails.rb
+++ lib/minitest/rails.rb
@@ -94,23 +94,6 @@ class ActionDispatch::IntegrationTest
   # Register by name, consider Acceptance to be deprecated
   register_spec_type(/(Integration|Acceptance)( ?Test)?\z/i, self)
 end
-
-require 'rails/generators/test_case'
-class Rails::Generators::TestCase
-  register_spec_type(self) do |desc|
-    Class === desc && desc < Rails::Generators::Base
-  end
-
-  register_spec_type(/Generator( ?Test)?\z/i, self)
-
-  def self.determine_default_generator(name)
-    generator = determine_constant_from_test_name(name) do |constant|
-      Class === constant && constant < Rails::Generators::Base
-    end
-    raise NameError.new("Unable to resolve generator for #{name}") if generator.nil?
-    generator
-  end
-end
 ################################################################################
 # Assertions and Expectations
 ################################################################################
