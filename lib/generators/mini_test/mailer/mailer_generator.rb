f0783fb76cec69941de65e71d14b14a6a59e8be6
Mike Moore <mike@blowmage.com>
2013-11-14 08:34:57 -0700

    Add namespace for mailer generator

--- lib/generators/mini_test/mailer/mailer_generator.rb
+++ lib/generators/mini_test/mailer/mailer_generator.rb
@@ -9,9 +9,9 @@ module MiniTest
 
       def create_test_files
         if options[:spec]
-          template "mailer_spec.rb", "test/mailers/#{file_name}_test.rb"
+          template "mailer_spec.rb", File.join("test","mailers", class_path, "#{file_name}_test.rb")
         else
-          template "mailer_test.rb", "test/mailers/#{file_name}_test.rb"
+          template "mailer_test.rb", File.join("test","mailers", class_path, "#{file_name}_test.rb")
         end
       end
     end
--- test/generators/test_mailer_generator.rb
+++ test/generators/test_mailer_generator.rb
@@ -12,6 +12,15 @@ class TestMailerGenerator < GeneratorTest
     assert_match(/class NotificationTest/m, contents)
   end
 
+  def test_namespaced_mailer_generator
+    assert_output(/create  test\/mailers\/admin\/notification_test.rb/m) do
+      MiniTest::Generators::MailerGenerator.start ["admin/notification"]
+    end
+    assert File.exists? "test/mailers/admin/notification_test.rb"
+    contents = File.read "test/mailers/admin/notification_test.rb"
+    assert_match(/class Admin::NotificationTest/m, contents)
+  end
+
   def test_mailer_generator_spec
     assert_output(/create  test\/mailers\/notification_test.rb/m) do
       MiniTest::Generators::MailerGenerator.start ["notification", "welcome", "--spec"]
@@ -20,5 +29,15 @@ class TestMailerGenerator < GeneratorTest
     contents = File.read "test/mailers/notification_test.rb"
     assert_match(/describe Notification do/m, contents)
   end
+
+  def test_namespaced_mailer_generator_spec
+    assert_output(/create  test\/mailers\/admin\/notification_test.rb/m) do
+      MiniTest::Generators::MailerGenerator.start ["admin/notification", "welcome", "--spec"]
+    end
+    assert File.exists? "test/mailers/admin/notification_test.rb"
+    contents = File.read "test/mailers/admin/notification_test.rb"
+    assert_match(/describe Admin::Notification do/m, contents)
+  end
+
 
 end
