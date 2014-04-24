bead31ea13b2685d1c5baff9b03cef4088bc76cb
Mike Moore <mike@blowmage.com>
2014-04-24 17:27:05 -0600

    Set RAILS_ENV in railtie [fixes #144]
    If users don't include the default testing engine, then RAILS_ENV can be wrong.
    If RAILS_ENV is development then tests will fail, so make sure it is correct.
    Update railtie and remove the generators check from Rails 3.0 while we are at it.

--- lib/minitest/rails/railtie.rb
+++ lib/minitest/rails/railtie.rb
@@ -1,12 +1,19 @@
+if defined?(Rake.application) && Rake.application.top_level_tasks.grep(/^test(?::|$)/).any?
+  ENV['RAILS_ENV'] ||= 'test'
+end
+
 require "rails"
 
 module Minitest
   module Rails
-    class Railtie < ::Rails::Railtie # :nodoc:
-      generators = config.respond_to?(:app_generators) ? config.app_generators : config.generators
-      generators.integration_tool :minitest
-      generators.test_framework   :minitest
-      generators.fallbacks[:minitest] = :test_unit
+    class Railtie < ::Rails::Railtie
+      config.app_generators do |c|
+        c.test_framework :minitest,
+                         fixture: true,
+                         fixture_replacement: nil
+        c.integration_tool :minitest
+        c.fallbacks[:minitest] = :test_unit
+      end
 
       rake_tasks do
         load "rails/test_unit/testing.rake"
