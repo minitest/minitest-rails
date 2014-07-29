a6ed8a0a51c23c72b412bc90dcc5efbdd5025a4a
Isaac Sloan <isaac@isaacsloan.com>
2014-07-29 00:46:04 -0600

    Fixed scaffold generator so that it will pass on unchanged scaffolding.
    added tests to prove that it wasn't calling none existent instance variables

--- test/generators/test_scaffold_generator.rb
+++ test/generators/test_scaffold_generator.rb
@@ -5,20 +5,22 @@ class TestScaffoldGenerator < GeneratorTest
 
   def test_scaffold_generator
     assert_output(/create  test\/controllers\/users_controller_test.rb/m) do
-      Minitest::Generators::ScaffoldGenerator.start ["user"]
+      Minitest::Generators::ScaffoldGenerator.start ["User", "name:string", "email:string"]
     end
     assert File.exists? "test/controllers/users_controller_test.rb"
     contents = File.read "test/controllers/users_controller_test.rb"
     assert_match(/class UsersControllerTest/m, contents)
+    assert_match(/post :create, user: { email: user.email, name: user.name }/m, contents)
   end
 
   def test_scaffold_generator_spec
     assert_output(/create  test\/controllers\/users_controller_test.rb/m) do
-      Minitest::Generators::ScaffoldGenerator.start ["user", "--spec"]
+      Minitest::Generators::ScaffoldGenerator.start ["User", "name:string", "email:string", "--spec"]
     end
     assert File.exists? "test/controllers/users_controller_test.rb"
     contents = File.read "test/controllers/users_controller_test.rb"
     assert_match(/describe UsersController do/m, contents)
+    assert_match(/post :create, user: { email: user.email, name: user.name }/m, contents)
   end
 
 end
