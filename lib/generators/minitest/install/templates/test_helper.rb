39b852515960760f0893c768d25915941c0cb4d6
Adrian Mugnolo <adrian@mugnolo.com>
2015-10-08 00:32:15 -0200

    Update template
    Sync template from rails/master.

--- lib/generators/minitest/install/templates/test_helper.rb
+++ lib/generators/minitest/install/templates/test_helper.rb
@@ -1,6 +1,6 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path("../../config/environment", __FILE__)
-require "rails/test_help"
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
 require "minitest/rails"
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
@@ -14,6 +14,7 @@ class ActiveSupport::TestCase
 <% unless options[:skip_active_record] -%>
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
 <% end -%>
   # Add more helper methods to be used by all tests here...
 end
--- test/generators/test_install_generator.rb
+++ test/generators/test_install_generator.rb
@@ -9,7 +9,7 @@ class TestInstallGenerator < GeneratorTest
     end
     assert File.exists? "test/test_helper.rb"
     contents = File.read "test/test_helper.rb"
-    assert_match(/require "rails\/test_help"/m, contents)
+    assert_match(/require 'rails\/test_help'/m, contents)
     assert_match(/require "minitest\/rails"/m, contents)
     assert_match(/fixtures :all/m, contents)
   end
@@ -20,7 +20,7 @@ class TestInstallGenerator < GeneratorTest
     end
     assert File.exists? "test/test_helper.rb"
     contents = File.read "test/test_helper.rb"
-    assert_match(/require "rails\/test_help"/m, contents)
+    assert_match(/require 'rails\/test_help'/m, contents)
     assert_match(/require "minitest\/rails"/m, contents)
     refute_match(/fixtures :all/m, contents)
   end
